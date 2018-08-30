78cccd12260acdc913865fc66c2764c64a76027a
Cali Castle <cali@calicastle.com>
2018-08-30 00:00:49 -0700

    First run through

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,24 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using JetBrains.Annotations;
 using UnityEngine;
-using Microsoft;
 using Microsoft.Mixer;
+using UnityEngine.UI;
 
-public class Game : MonoBehaviour {
+public class Game : MonoBehaviour
+{
+    public Text text;
 
-	// Use this for initialization
-	void Start () {
-		// Call GoInteractive to connect to the Mixer service so you can start
-		// recieving input.
-		MixerInteractive.GoInteractive();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (MixerInteractive.GetButton("GiveHealth"))
-		{
-			Debug.Log("It's triggered!");
-		}
-	}
+    // Initialization
+    private void Start()
+    {
+        // Connect to Mixer
+        MixerInteractive.GoInteractive();
+        // Listen for interactive button events
+        MixerInteractive.OnInteractiveButtonEvent += OnInteractiveButtonEvent;
+        MixerInteractive.OnInteractiveTextControlEvent += OnInteractiveTextControlEvent;
+    }
+
+    /// <summary>
+    ///	When an interactive button event is received.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public void OnInteractiveButtonEvent(object sender, InteractiveButtonEventArgs e)
+    {
+        Debug.Log(e.ControlID + " was clicked.");
+        
+        e.CaptureTransaction();
+    }
+
+    public void OnInteractiveTextControlEvent(object sender, InteractiveTextEventArgs e)
+    {
+        Debug.Log(e.ControlID + " was typed.");
+        
+        text.text = e.Text;
+    }
 }
--- MixerControls/mixer-vue-cdk/src/scripts.js
+++ MixerControls/mixer-vue-cdk/src/scripts.js
@@ -1,22 +1,27 @@
-window.addEventListener('load', function initMixer() {
-  // Move the video by a static offset amount
-  const offset = 50;
-  mixer.display.moveVideo({
-    top: offset,
-    bottom: offset,
-    left: offset,
-    right: offset,
-  });
-
-  // Whenever someone clicks on "Hello World", we'll send an event
-  // to the game client on the control ID "hello-world"
-  document.getElementById('hello-world').onclick = function (event) {
-    mixer.socket.call('giveInput', {
-      controlID: 'hello',
-      event: 'click',
-      button: event.button,
-    });
-  };
-
-  mixer.isLoaded();
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Mixer + Vue!',
+  },
+  watch: {
+    message: function (newValue, oldValue) {
+      mixer.socket.call("giveInput", {
+        event: "click",
+        text: newValue,
+        controlID: "new-text"
+      });
+    }
+  },
+  methods: {
+    clickButton: function () {
+      mixer.socket.call("giveInput", {
+        event: "click",
+        health: 2,
+        controlID: "new-control"
+      })
+    }
+  },
+  mounted() {
+    mixer.isLoaded();
+  }
 });
